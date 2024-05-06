bf2faf0faaa8c9ec6be5d597310e4ab7f6a74390
Maicon Riebe Gretschmann <71952493+MaiconRG@users.noreply.github.com>
2024-05-06 14:10:41 -0300

    Atualização de Dispositivos moveis
    Funcionalidades JS

--- script/script.js
+++ script/script.js
@@ -1,35 +1,46 @@
 // Mostrando Div e Escondendo
 var botao  = document.getElementById("btn_design");
-
 botao.addEventListener("click", function(){
 
+    var verificar1 = document.getElementById("pagina_certificados");
+    var verificar2 = document.getElementById("pagina_desenvolvimento")
+
+    if(verificar1, verificar2.className != "hide"){
+        verificar1.classList.add("hide");
+        verificar2.classList.add("hide");
+    }
+    
     var container = document.getElementById("pagina_design");
     container.classList.toggle("hide");
-
 })
-
-
-
-
 
 var botao  = document.getElementById("btn_certificados");
-
 botao.addEventListener("click", function(){
 
+    var verificar1 = document.getElementById("pagina_design");
+    var verificar2 = document.getElementById("pagina_desenvolvimento")
+
+    if(verificar1, verificar2.className != "hide"){
+        verificar1.classList.add("hide");
+        verificar2.classList.add("hide");
+    }
     var container = document.getElementById("pagina_certificados");
     container.classList.toggle("hide");
-
 })
 
-
-
 var botao  = document.getElementById("btn_desenvolvimento");
-
 botao.addEventListener("click", function(){
 
+    var verificar1 = document.getElementById("pagina_certificados");
+    var verificar2 = document.getElementById("pagina_design")
+
+    if(verificar1, verificar2.className != "hide"){
+        verificar1.classList.add("hide");
+        verificar2.classList.add("hide");
+    }
+          
     var container = document.getElementById("pagina_desenvolvimento");
     container.classList.toggle("hide");
-
 })
 
 /*
